aa858d3d5656fe00a47419606326bdaee8b57129
Your Name <you@example.com>
2020-02-29 00:24:07 +0530

    First Commit

---
+++ test/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\pollster_django_crash-master\pollster_django_crash-master\test\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
